56ed5597d0e4dbaa1c46b9d412cbc3f14341e93e
speaslee <sophiapeaslee@gmail.com>
2025-12-12 20:01:14 -0700

    add changing posters

--- assets/js/posters_slideshow.js
+++ assets/js/posters_slideshow.js
@@ -1,14 +1,31 @@
 var slideIndex = 0;
-carousel();
+var carouselRunning = false;
 
 function carousel() {
+  if (carouselRunning) return;
+  carouselRunning = true;
+  
   var i;
   var x = document.getElementsByClassName("posterImages");
+  
+  if (x.length === 0) {
+    carouselRunning = false;
+    return;
+  }
+  
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 3 seconds
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  
+  x[slideIndex - 1].style.display = "block";
+  
+  carouselRunning = false;
+  setTimeout(carousel, 5000);
 }
+
+carousel();
